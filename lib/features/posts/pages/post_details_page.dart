8d1d32e5c91f3a213b281f5db0b6435bbc317926
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-07-28 12:19:58 +0530

    created base folder structure

---
+++ lib/features/posts/pages/post_details_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostDetailsPage extends StatefulWidget {
+  const PostDetailsPage({super.key});
+
+  @override
+  State<PostDetailsPage> createState() => _PostDetailsPageState();
+}
+
+class _PostDetailsPageState extends State<PostDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
