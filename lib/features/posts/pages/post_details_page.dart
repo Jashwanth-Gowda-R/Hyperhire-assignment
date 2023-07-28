74633648b1bfee96703306e12d24232ad993ad90
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-07-28 14:35:07 +0530

    added details in post

--- lib/features/posts/pages/post_details_page.dart
+++ lib/features/posts/pages/post_details_page.dart
@@ -10,6 +10,15 @@ class PostDetailsPage extends StatefulWidget {
 }
 
 class _PostDetailsPageState extends State<PostDetailsPage> {
+  final String details = '''\n지난 월요일에 2023년 S/S 트렌드 알아보기 이벤트 참석했던 팝들아~
+혹시 어떤 상품이 제일 괜찮았어? 
+
+후기 올라오는거 보면 로우라이즈? 그게 제일 반응 좋고 그 테이블이 
+제일 재밌었다던데 맞아???
+
+올해 로우라이즈가 트렌드라길래 나도 도전해보고 싶은데 말라깽이가
+아닌 사람들도 잘 어울릴지 너무너무 궁금해ㅜㅜ로우라이즈 테이블에
+있었던 팝들 있으면 어땠는지 후기 좀 공유해주라~~! ''';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +42,9 @@ class _PostDetailsPageState extends State<PostDetailsPage> {
       body: Container(
         padding: const EdgeInsets.only(
           top: 20,
+          right: 0,
+          left: 0,
+          bottom: 0,
         ),
         child: Column(
           children: [
@@ -49,7 +61,35 @@ class _PostDetailsPageState extends State<PostDetailsPage> {
                 btntitle: "팔로우",
               ),
             ),
-
+            Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.only(
+                left: 25,
+                right: 20,
+              ),
+              child: RichText(
+                text: TextSpan(
+                  text: '지난 월요일에 했던 이벤트 중 가장 괜찮은 상품 뭐야?\n',
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 14,
+                    fontFamily: 'Noto Sans',
+                  ),
+                  children: [
+                    TextSpan(
+                      text: details,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 12,
+                        fontFamily: 'Noto Sans',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
